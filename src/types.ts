6ed3951755f51634e161ff76a9c92428a59df300
ootvt <39486253+ootvt@users.noreply.github.com>
2025-12-02 20:46:54 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,4 @@
 import Parser from 'rss-parser';
-import DB_PROPERTIES from '../cols.json';
-
-export type DB_PROPERTIES_KEYS = Exclude<keyof typeof DB_PROPERTIES, "NAME">;
 
 export enum ItemCategory {
   Movie = 'movie',
@@ -30,91 +27,4 @@ export type FeedItem = {
   time: string;
   status: ItemStatus;
   category: ItemCategory;
-};
-
-export enum NotionPropTypesEnum {
-  TITLE = 'title',
-  RICH_TEXT = 'rich_text',
-  FILES = 'files',
-  DATE = 'date',
-  MULTI_SELECT = 'multi_select',
-  NUMBER = 'number',
-  URL = 'url',
-};
-
-export type NotionRichTextPropType = {
-  id?: string;
-  type: NotionPropTypesEnum.RICH_TEXT;
-  rich_text: {
-    type: 'text';
-    text: {
-      content: string;
-    };
-  }[];
-};
-
-export type NotionTitlePropType = {
-  id?: 'title';
-  type: NotionPropTypesEnum.TITLE;
-  title: {
-    text: {
-      content: string;
-    };
-  }[];
-};
-
-export type NotionFilesPropType = {
-  id?: string;
-  type: NotionPropTypesEnum.FILES;
-  files: {
-    name: string;
-    type: 'external';
-    external: {
-      url: string;
-    };
-  }[];
-};
-
-export type NotionDatePropType = {
-  id?: string;
-  type: NotionPropTypesEnum.DATE;
-  date: {
-    start: string;
-    end: string | null;
-    time_zone: string | null;
-  };
-};
-
-export type NotionMultiSelectPropType = {
-  id?: string;
-  type: NotionPropTypesEnum.MULTI_SELECT;
-  multi_select: {
-    name: string;
-  }[];
-};
-
-export type NotionNumberPropType = {
-  id?: string;
-  type: NotionPropTypesEnum.NUMBER;
-  number: number | null;
-};
-
-export type NotionUrlPropType = {
-  id?: string;
-  type: NotionPropTypesEnum.URL;
-  url: string;
-};
-
-export type NotionColPropTypes =
-  | NotionRichTextPropType
-  | NotionTitlePropType
-  | NotionFilesPropType
-  | NotionDatePropType
-  | NotionMultiSelectPropType
-  | NotionNumberPropType
-  | NotionUrlPropType;
-
-export type FailedItem = {
-  link: string;
-  title: string;
 };
